7879b488dbf8cebb86a294a042e880d751990256
vtella <rtella@hotmail.com>
2020-10-21 17:37:13 -0500

    Add files via upload

---
+++ cfdi_complemento_leyendas_fiscales_ee/__init__.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    All Rights Reserved.
+#    info skype: german_442 email: (german.ponce@argil.mx)
+############################################################################
+#    Coded by: german_442 email: (german.ponce@argil.mx)
+
+##############################################################################
+
+# import utils
+from . import models
---
+++ cfdi_complemento_leyendas_fiscales_ee/__manifest__.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    All Rights Reserved.
+#    info skype: german_442 email: (german.ponce@argil.mx)
+############################################################################
+#    Coded by: german_442 email: (german.ponce@argil.mx)
+
+##############################################################################
+
+{
+    'name' : 'Complemento - Leyendas Fiscales E.E.',
+    'description' : """ 
+
+    Este Modulo Permite incorporar el Complemento de Leyendas Fiscales al generar Facturas CFDI.
+
+    Configuracion
+
+    - Activamos el Campo Leyendas Fiscales, en la Ficha de Clientes.
+
+    - Al generar la Factura en el Sistema, el campo Comentario (Comment), debe incluir el texto que se llevara al nodo Comentario, en la generacion del Complemento.
+
+
+    """,
+    'version' : '1.0',
+    'author' : 'German Ponce Dominguez',
+    'website' : 'http://www.argil.mx',
+    'license' : 'GPL-3',
+    'category' : 'Nishikawa',
+    'depends' : [
+                'account',
+                'sale',
+                'l10n_mx_edi',
+                ],
+    'init_xml' : [],
+    'data' : [
+                    'views/res_partner_view.xml',
+                    'views/extra_fit_view.xml',
+                    'data/complemento_view.xml',
+                    'security/ir.model.access.csv'
+                    ],
+    'demo_xml' : [],
+    'installable' : True,
+    'active' : False
+}
